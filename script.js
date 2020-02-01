af66983de1f3d8b7ded0c68a307383de1e985012
Kielx <Kiel66@gmail.com>
2020-02-01 21:40:34 +0100

    add scripting for black background and random color background

--- script.js
+++ script.js
@@ -1,17 +1,38 @@
-for(let i = 0; i<256; i++){
-    let newDiv = document.createElement("div");
-    newDiv.classList.add(`box`);
-    newDiv.classList.add(`newDiv${i}`);
-    let main = document.querySelector("main");
-    main.appendChild(newDiv);
+const createGrid = function(size){
+    let roz = Math.sqrt(size);
+    for(let i = 0; i<size; i++){
+        let newDiv = document.createElement("div");
+        newDiv.classList.add(`box`);
+        newDiv.classList.add(`newDiv${i}`);
+        let main = document.querySelector("main");
+        main.appendChild(newDiv);
+        newDiv.style = `width: calc(100%/${roz}); height: calc(100%/${roz})`;
+    }
+};
 
-}
+const blackBackground = function(){
+    let boxlist = document.querySelectorAll(".box");
+    boxlist.forEach(box => {
+        box.addEventListener("mouseover", () => {
+            event.target.classList.add("box-black");
+        });
+    });
+};
 
-let box = document.querySelectorAll(".box");
-box.forEach(content => {
-    content.addEventListener("mouseover", () => {
-        content.style.backgroundColor = 'black';
-    })
-});
+const randomBackground = function(){
 
-console.log(box)
+    let boxlist = document.querySelectorAll(".box");
+    boxlist.forEach(box => {
+        let x = Math.floor(Math.random() * 256);
+        let y = Math.floor(Math.random() * 256);
+        let z = Math.floor(Math.random() * 256);
+        box.addEventListener("mouseover", () => {
+            event.target.style.backgroundColor = `rgb(${x}, ${y}, ${z})`;
+        });
+    });
+};
+
+
+createGrid(100);
+randomBackground();
+
