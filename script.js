60e29be77aeb8e27453faabe69363e3cf68e5077
Kielx <Kiel66@gmail.com>
2020-02-01 23:26:05 +0100

    finished ver 0.1

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 const createGrid = function(size) {
   let roz = Math.sqrt(size);
-  for (let i = 0; i < size; i++) {
+  for (let i = 0; i < size*size; i++) {
     let newDiv = document.createElement("div");
     newDiv.classList.add(`box`);
     newDiv.classList.add(`newDiv${i}`);
     let main = document.querySelector("main");
     main.appendChild(newDiv);
-    newDiv.style = `width: calc(100%/${roz}); height: calc(100%/${roz})`;
+    newDiv.style = `width: calc(100%/${size}); height: calc(100%/${size})`;
   }
 };
 
 const blackBackground = function() {
+    makeGrid()
   let boxlist = document.querySelectorAll(".box");
   boxlist.forEach(box => {
     box.addEventListener("mouseover", () => {
@@ -19,7 +20,8 @@ const blackBackground = function() {
   });
 };
 
-const randomBackground = function() {
+const stickyRandomBackground = function() {
+    makeGrid()
   let boxlist = document.querySelectorAll(".box");
   boxlist.forEach(box => {
     let x = Math.floor(Math.random() * 256);
@@ -31,8 +33,53 @@ const randomBackground = function() {
   });
 };
 
+const randomBackground = function() {
+    makeGrid()
+    let boxlist = document.querySelectorAll(".box");
+    boxlist.forEach(box => {
+
+      box.addEventListener("mouseover", () => {
+        let x = Math.floor(Math.random() * 256);
+        let y = Math.floor(Math.random() * 256);
+        let z = Math.floor(Math.random() * 256);
+        event.target.style.opacity = 1;
+        event.target.style.backgroundColor = `rgb(${x}, ${y}, ${z})`;
+      });
+    });
+  };
+
+  const shading = function() {
+    makeGrid();
+    let boxlist = document.querySelectorAll(".box");
+    boxlist.forEach(box => {
+      box.addEventListener("mouseover", () => {
+        event.target.style.backgroundColor = 'gray';
+        event.target.style.opacity = Number(event.target.style.opacity) + 0.1;
+        event.target.style.border = '1px solid black';
+      });
+    });
+  };
+
+
+const makeGrid = function(){
+    document.querySelector('main').textContent = '';
+    let gridSize = document.getElementById("grid-size").value;
+    createGrid(gridSize);};
+
+
+
 const buttonBlack = document.getElementById("black-button");
 buttonBlack.addEventListener("click", blackBackground);
+
+const stickyButtonRandom = document.getElementById("sticky-random-button");
+stickyButtonRandom.addEventListener("click", stickyRandomBackground);
+
 const buttonRandom = document.getElementById("random-button");
 buttonRandom.addEventListener("click", randomBackground);
-createGrid(100);
+
+const buttonShading = document.getElementById("shading-button");
+buttonShading.addEventListener("click", shading);
+
+
+const buttonCreateGrid = document.getElementById("create-button");
+buttonCreateGrid.addEventListener("click", makeGrid)
